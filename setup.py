7d48db2738202787b73b4aa40decf707d7c21196
Saverio <saver.mucci@gmail.com>
2016-04-22 10:21:19 +0200

    change package name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='countries-from-coordinates',
+    name='countries_from_coordinates',
     version='0.0.1',
     description='A simple package to read coordinates files and transform them to countries',
     long_description=long_description,
